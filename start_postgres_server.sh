cc14c4f371b12d3ca1e09527797a264444ffbc6c
kuuuube <hexagonisalie@gmail.com>
2025-10-12 10:17:26 -0400

    Note running as postgres user possibly failing

--- start_postgres_server.sh
+++ start_postgres_server.sh
@@ -1,3 +1,8 @@
 sudo mkdir /run/postgresql
 sudo chown postgres /run/postgresql
+
+# if this command doesn't work switch to the postgres user and run it instead
+# ex:
+# sudo su - postgres
+# pg_ctl -D /var/lib/postgres/data -l logfile start
 sudo -u postgres pg_ctl -D /var/lib/postgres/data -l logfile start
