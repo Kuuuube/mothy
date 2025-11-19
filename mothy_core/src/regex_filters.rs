51a66eeb555189cbb92427b4384c1a631275132f
kuuuube <hexagonisalie@gmail.com>
2025-11-18 20:39:03 -0500

    Remove zero length regex

--- mothy_core/src/regex_filters.rs
+++ mothy_core/src/regex_filters.rs
@@ -2,6 +2,7 @@ pub fn init() -> Vec<regex::Regex> {
     let regex_strs = include_str!("regex_filters.txt").split("\n");
     let regex_comps: Vec<regex::Regex> = regex_strs
     .clone()
+    .filter(|x| x.trim().len() > 0)
     .map(|x| {
         regex::RegexBuilder::new(x)
             .case_insensitive(true)
