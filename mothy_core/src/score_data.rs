c04d1706cc38e4a24a4e6b4bcc6b81a575544ab8
kuuuube <hexagonisalie@gmail.com>
2026-02-22 20:48:05 -0500

    Remove dbg

--- mothy_core/src/score_data.rs
+++ mothy_core/src/score_data.rs
@@ -4,7 +4,6 @@ pub fn init() -> Result<Vec<ScoresData>, Box<dyn std::error::Error>> {
     let scores_file_decompressed =
         decode_zstd(include_bytes!("../../assets/scores_data.json.zst")).unwrap();
     let scores_file_str = str::from_utf8(&scores_file_decompressed).unwrap();
-    // dbg!(scores_file_str);
 
     let scores_data: Vec<ScoresData> = serde_json::from_str(scores_file_str).unwrap();
     return Ok(scores_data);
