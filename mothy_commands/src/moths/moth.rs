ca324c6d0238d806a8a2f71951e29d08eaabc5b5
kuuuube <hexagonisalie@gmail.com>
2026-02-22 22:38:08 -0500

    Add most info to moth command

--- mothy_commands/src/moths/moth.rs
+++ mothy_commands/src/moths/moth.rs
@@ -18,12 +18,54 @@ pub async fn moth(ctx: Context<'_>) -> Result<(), Error> {
         let mut rng = rand::rng();
         data.moth_data.choose(&mut rng).unwrap()
     };
+    let classifications = moth.classification.clone();
 
-    let title = format!("{} {}", moth.classification.genus, moth.classification.epithet);
-    let embed = serenity::CreateEmbed::default().title(title);
+    let title = format!("{} {}", classifications.genus, classifications.epithet);
+
+    let mut fields = vec![];
+    if let Some(common_names) = &moth.common_names {
+        fields.push(("Common Names", common_names.join(", "), false));
+    }
+    let moth_rank_flow = [
+        get_moth_rank_vec(&[
+            classifications.superfamily,
+            classifications.family,
+            classifications.subfamily,
+            classifications.tribe,
+            classifications.subtribe,
+        ]),
+        vec![classifications.genus, classifications.epithet],
+    ]
+    .concat()
+    .join(" -> ");
+    fields.push(("Taxa", moth_rank_flow, false));
+
+    let footer = serenity::CreateEmbedFooter::new(format!(
+        "Catalog of Life ID {}",
+        moth.catalogue_of_life_taxon_id
+    ));
+
+    let embed = serenity::CreateEmbed::default()
+        .title(title)
+        .url(format!(
+            "https://www.catalogueoflife.org/data/taxon/{}",
+            moth.catalogue_of_life_taxon_id
+        ))
+        .fields(fields)
+        .footer(footer);
     ctx.send(poise::CreateReply::default().embed(embed)).await?;
 
     Ok(())
+}
+
+fn get_moth_rank_vec(input_strings: &[Option<String>]) -> Vec<String> {
+    let mut ranks_vec = Vec::new();
+    for input_string in input_strings {
+        if let Some(some) = input_string {
+            ranks_vec.push(some.to_string());
+        }
+    }
+    return ranks_vec;
 }
 
 #[must_use]
