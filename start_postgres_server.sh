7ab18efe944dcec2856af553945fc45af9b82e50
kuuuube <hexagonisalie@gmail.com>
2026-02-22 20:30:37 -0500

    Make start script echo commands to run if it doesnt work

--- start_postgres_server.sh
+++ start_postgres_server.sh
@@ -1,9 +1,13 @@
 sudo mkdir /run/postgresql
 sudo chown postgres /run/postgresql
 
-# if this command doesn't work switch to the postgres user and run it instead
-# ex:
-# sudo su - postgres
-# pg_ctl -D /var/lib/postgres/data -l logfile start
-# exit
+echo
+echo If the following command doesn\'t work switch to the postgres user and run it instead
+echo ex:
+echo \`\`\`
+echo $ sudo su - postgres
+echo $ pg_ctl -D /var/lib/postgres/data -l logfile start
+echo $ exit
+echo \`\`\`
+echo
 sudo -u postgres pg_ctl -D /var/lib/postgres/data -l logfile start
